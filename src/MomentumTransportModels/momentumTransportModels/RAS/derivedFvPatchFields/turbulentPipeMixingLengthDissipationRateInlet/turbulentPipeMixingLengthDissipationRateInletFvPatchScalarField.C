d19995ac61b90b62b26acf7e069c64f269dfa806
SvendB3nt <mathias.poulsen.91@gmail.com>
2025-12-03 16:09:24 +0100

    Added decriptions to header files

---
+++ src/MomentumTransportModels/momentumTransportModels/RAS/derivedFvPatchFields/turbulentPipeMixingLengthDissipationRateInlet/turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField.C
@@ -0,0 +1,179 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fieldMapper.H"
+#include "surfaceFields.H"
+#include "volFields.H"
+#include "momentumTransportModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField::
+turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    inletOutletPipeFvPatchField(p, iF),
+    turbulentLengthScale_(dict.lookupOrDefault<scalar>("L", 0.07)),
+    kName_(dict.lookupOrDefault<word>("k", "k")),
+    Cmu_(dict.lookupOrDefault<scalar>("Cmu", 0.09))
+{
+    this->phiName_ = dict.lookupOrDefault<word>("phi", "phi");
+    this->calcHydDia_= !dict.found("hydraulicDiameter");
+    this->hydraulicDiameter_ = dict.lookupOrDefault<scalar>("hydraulicDiameter", 0.0);
+    this->shape_ = dict.lookupOrDefault<word>("shape", "circle");
+    this->shapeRatio_ = 1.0; 
+    this->hydDiaFnc_ = nullptr;
+
+    this->checkInput(dict);
+
+    this->selectHydDiaFnc();
+
+    if (this->calcHydDia_)
+    {
+        this->updateHydDia();
+    }
+
+    fvPatchScalarField::operator=
+    (
+        scalarField("value", iF.dimensions(), dict, p.size())
+    );
+
+    this->refValue() = 0.0;
+    this->refGrad() = 0.0;
+    this->valueFraction() = 0.0;
+}
+
+
+turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField::
+turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField
+(
+    const turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fieldMapper& mapper
+)
+:
+    inletOutletPipeFvPatchField(ptf, p, iF, mapper),
+    turbulentLengthScale_(ptf.turbulentLengthScale_),
+    kName_(ptf.kName_),
+    Cmu_(ptf.Cmu_)
+{}
+
+
+turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField::
+turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField
+(
+    const turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField& ptf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    inletOutletPipeFvPatchField(ptf, iF),
+    turbulentLengthScale_(ptf.turbulentLengthScale_),
+    kName_(ptf.kName_),
+    Cmu_(ptf.Cmu_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    if (patch().boundaryMesh().mesh().dynamic())
+    {
+        this->updateHydDia();
+    }
+
+    const scalar Cmu75 = pow(Cmu_, 0.75);
+
+    const fvPatchScalarField& kp =
+        patch().lookupPatchField<volScalarField, scalar>(kName_);
+
+    const fvsPatchScalarField& phip =
+        patch().lookupPatchField<surfaceScalarField, scalar>(this->phiName_);
+
+    this->refValue() = 
+        Cmu75*kp*sqrt(kp)/(turbulentLengthScale_*this->hydraulicDiameter_);
+    
+    this->valueFraction() = neg(phip);
+
+    inletOutletPipeFvPatchScalarField::updateCoeffs();
+}
+
+
+void turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField::write
+(
+    Ostream& os
+) const
+{
+    fvPatchScalarField::write(os);
+    writeEntry(os, "L", turbulentLengthScale_);
+    if (!this->calcHydDia_)
+    {
+        writeEntry(os, "hydraulicDiameter", this->hydraulicDiameter_);
+    }
+
+    if (this->shape_ == "rectangle")
+    {
+        writeEntry(os, "shapeRatio", this->shapeRatio_);
+    }
+    
+    writeEntry(os, "Cmu", Cmu_);
+    writeEntryIfDifferent<word>(os, "shape", "circle", this->shape_);
+    writeEntryIfDifferent<word>(os, "k", "k", kName_);
+    writeEntryIfDifferent<word>(os, "phi", "phi", this->phiName_);
+    writeEntry(os, "value", *this);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePatchTypeField
+(
+    fvPatchScalarField,
+    turbulentPipeMixingLengthDissipationRateInletFvPatchScalarField
+);
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
